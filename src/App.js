887d2ee6269b9aae7b4da87643036594e7744a73
Oliver Regan <ollieregan1@gmail.com>
2023-01-25 12:11:08 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <a href="goformz://template/b6bfaa30-d593-4fd8-b5a1-73a78413007d">Open New Form</a>        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
